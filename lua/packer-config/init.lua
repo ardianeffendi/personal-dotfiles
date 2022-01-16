bdf291319b9f65bc7091b52feb7370f73589b80f
Ardian Maulana Effendi <ardian.effendi21@gmail.com>
2022-01-16 17:56:48 +0800

    init tracker nvim config

---
+++ lua/packer-config/init.lua
@@ -0,0 +1,8 @@
+return require('packer').startup(function()
+  use 'wbthomason/packer.nvim'
+  use 'EdenEast/nightfox.nvim'
+  use 'kyazdani42/nvim-web-devicons'
+  use 'kyazdani42/nvim-tree.lua'
+  use 'neovim/nvim-lspconfig'
+end)
+
---
+++ lua/settings/init.lua
@@ -0,0 +1,23 @@
+local set = vim.opt
+
+set.expandtab = true
+set.smarttab = true
+set.shiftwidth = 2
+set.tabstop = 2
+set.softtabstop = 2
+set.autoindent = true
+
+set.hlsearch = false
+set.incsearch = true
+set.ignorecase = true
+set.smartcase = true
+
+set.wrap = false
+set.scrolloff = 8
+set.fileencoding = 'utf-8'
+set.termguicolors = true
+
+set.relativenumber = true
+
+set.hidden = true
+
