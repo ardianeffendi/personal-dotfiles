8b957527248853caef304b3b557cd6bee2152948
Ardian Maulana Effendi <ardian.effendi21@gmail.com>
2022-01-19 06:50:20 +0800

    add key mapping for telescope

--- init.lua
+++ init.lua
@@ -3,6 +3,7 @@ require('mappings')
 require('colorschemes-config.nightfox')
 require('packer-config')
 require('statusbar')
+require('mappings')
 
 require('lsp-config.language-servers')
 require('lsp-config.nvim-cmp')
